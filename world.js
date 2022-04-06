398bf32b68c5a752233242fa9567c1cfc3dc3df0
Justin Gilman <eyeofmidas@gmail.com>
2022-04-06 15:10:49 -0700

    updating world to es6 class

--- world.js
+++ world.js
@@ -1,44 +1,53 @@
-var World = function() {
-	var roomObjs = [];
-	var items = [];
-	this.addZone = function(zoneIndex, zoneData) {
-		roomObjs[zoneIndex] = [];
-		items[zoneIndex] = [];
+class World {
+	constructor() {
+		this.roomObjs = []
+		this.items = []
+	}
+
+	addZone(zoneIndex, zoneData) {
+		this.roomObjs[zoneIndex] = []
+		this.items[zoneIndex] = []
 		for (var i = 0; i < zoneData.length; i++) {
-			roomObjs[zoneIndex][i] = new zoneData[i]();
-			items[zoneIndex][i] = [];
+			this.roomObjs[zoneIndex][i] = new zoneData[i]()
+			this.items[zoneIndex][i] = []
 		}
-	};
-	this.getRoom = function(zone, room) {
-		return roomObjs[zone][room];
-	};
-	this.addItems = function(zone, room, roomItems) {
+	}
+
+	getRoom(zone, room) {
+		return this.roomObjs[zone][room]
+	}
+
+	addItems(zone, room, roomItems) {
 		for (var i = 0; i < roomItems.length; i++) {
-			var roomItem = new roomItems[i]();
-			items[zone][room].push(roomItem);
+			var roomItem = new roomItems[i]()
+			this.items[zone][room].push(roomItem)
 		}
-	};
-	this.printItems = function(zone, room) {
-		var roomItems = items[zone][room];
+	}
+
+	printItems(zone, room) {
+		var roomItems = this.items[zone][room]
 		for (var i = 0; i < roomItems.length; i++) {
-			roomItems[i].look();
+			roomItems[i].look()
 		}
-	};
-	this.getItems = function(zone, room) {
-		return items[zone][room];
-	};
-	this.takeItem = function(itemKey, pos) {
-		var roomItems = this.getItems(pos.zone, pos.room);
+	}
+
+	getItems(zone, room) {
+		return this.items[zone][room]
+	}
+
+	takeItem(itemKey, pos) {
+		var roomItems = this.getItems(pos.zone, pos.room)
 		for (var i = 0; i < roomItems.length; i++) {
 			if (system.in_array(itemKey, roomItems[i].keys)) {
-				var item = roomItems.splice(i, 1);
-				return item[0];
+				var item = roomItems.splice(i, 1)
+				return item[0]
 			}
 		}
-		return null;
-	};
-	this.giveItem = function(item, pos) {
-		items[pos.zone][pos.room].push(item);
-	};
-};
-var world = new World();
+		return null
+	}
+	giveItem(item, pos) {
+		this.items[pos.zone][pos.room].push(item)
+	}
+}
+
+var world = new World()
