e7c9780a3240ac2d0c9b22fda555fbc60bb73fdc
Justin Gilman <eyeofmidas@gmail.com>
2022-04-07 10:34:25 -0700

    consolidating all "engine" classes into core.js file

--- core.js
+++ core.js
@@ -1,3 +1,46 @@
+class World {
+	constructor() {
+		this.roomObjs = {}
+		this.items = {}
+	}
+
+	addZone(zone, zoneData) {
+		this.roomObjs[zone] = []
+		this.items[zone] = []
+		zoneData.forEach(roomClass => {
+			this.roomObjs[zone][roomClass.name] = new roomClass()
+			this.items[zone][roomClass.name] = []
+		})
+	}
+
+	getRoom(zone, room) {
+		return this.roomObjs[zone][room.name]
+	}
+
+	addItems(zone, room, roomItems) {
+		roomItems.forEach(itemClass => {
+			this.items[zone][room.name].push(new itemClass())
+		})
+	}
+
+	getItems(zone, room) {
+		return this.items[zone][room.name]
+	}
+
+	takeItem(itemKey, pos) {
+		let roomItems = this.getItems(pos.zone, pos.room)
+		for (let i = 0; i < roomItems.length; i++) {
+			if (roomItems[i].keys.includes(itemKey)) {
+				let item = roomItems.splice(i, 1)
+				return item[0]
+			}
+		}
+		return null
+	}
+	giveItem(item, pos) {
+		this.items[pos.zone][pos.room.name].push(item)
+	}
+}
 
 class RoomCore {
     constructor(title, description, exitsDescription) {
@@ -123,5 +166,209 @@ class ItemCore {
 		}
 		return false
 	}
-
 }
+
+class Player {
+	constructor() {
+		this.currentZone = null
+		this.currentRoom = null
+		this.playerInventory = []
+	}
+
+	actions(command, commandData) {
+		if (typeof (this[command]) == "function") {
+			return this[command](commandData)
+		}
+		return false
+	}
+
+	look(commandData) {
+		if (commandData.length <= 0) {
+			system.println("")
+			return true
+		}
+		return false
+	}
+
+	l = this.look
+	
+	inventory() {
+		if (this.playerInventory.length <= 0) {
+			system.println("You are not carrying anything.")
+		} else {
+			system.println("You are holding:")
+			for (var i = 0; i < this.playerInventory.length; i++) {
+				this.playerInventory[i].held()
+			}
+		}
+		system.println("")
+		return true
+	}
+
+	i = this.inventory
+
+	getPosition() {
+		return {
+			zone: this.currentZone,
+			room: this.currentRoom
+		}
+	}
+
+	setPosition(zone, room) {
+		this.currentZone = zone
+		this.currentRoom = room
+		world.getRoom(zone, room).enter()
+		var items = world.getItems(zone, room)
+		for (var i = 0; i < items.length; i++) {
+			items[i].look([])
+		}
+		system.println("")
+	}
+
+	getInventory() {
+		return this.playerInventory
+	}
+
+	giveItem(item) {
+		this.playerInventory.push(item)
+	}
+	takeItem(itemKey) {
+		for (var i = 0; i < this.playerInventory.length; i++) {
+			if (this.playerInventory[i].keys.includes(itemKey)) {
+				var item = this.playerInventory.splice(i, 1)
+				return item[0]
+			}
+		}
+		return null
+	}
+}
+
+class System {
+	constructor() {
+		this.outputDiv = window.document.getElementById("output")
+		this.inputField = window.document.getElementById("input")
+		this.submitButton = window.document.getElementById("submit")
+	}
+
+	init() {
+		this.submitButton.addEventListener('click', () => {
+			this.parseInput()
+		}, false)
+
+		document.body.addEventListener("keyup", (event) => {
+			switch (event.key) {
+				case "Enter":
+					this.parseInput()
+					break;
+			}
+		}, false)
+
+		this.clear()
+		player.setPosition("OldHouse", OldHouse_EntryHall)
+
+	}
+
+	read() {
+		return this.inputField.value
+	}
+
+	print(message) {
+		this.outputDiv.innerHTML += message
+	}
+
+	println(message) {
+		this.print(message + "<br />\n")
+	}
+
+    log() {
+        [...arguments].forEach(argument => {
+            this.print(JSON.stringify(argument))
+        })
+        this.println("")
+    }
+
+	clear() {
+		this.outputDiv.innerHTML = ""
+		this.clearInput()
+	}
+
+	clearInput() {
+		this.inputField.value = ""
+		this.inputField.focus()
+		this.outputDiv.scrollTop = this.outputDiv.scrollHeight
+	}
+
+	parseInput() {
+		let commandData = this.read().split(' ')
+		let command = commandData.splice(0, 1)
+		command = command[0]
+		let playerPos = player.getPosition()
+		let room = world.getRoom(playerPos.zone, playerPos.room)
+
+		if (!room.actions(command, commandData)) {
+			if (!this.handleRoomItems(playerPos, command, commandData)) {
+				if (!player.actions(command, commandData)) {
+					if (!this.handleInventoryItems(command, commandData)) {
+						if (!this.actions(command, commandData)) {
+							this.println(`I don't recognize '${command}'.`)
+						}
+					}
+				}
+			}
+		}
+		this.clearInput()
+	}
+
+	handleRoomItems(playerPos, command, commandData) {
+		let roomItems = world.getItems(playerPos.zone, playerPos.room)
+		for (let i = 0; i < roomItems.length; i++) {
+			if (roomItems[i].actions(command, commandData)) {
+				return true
+			}
+		}
+		return false
+	}
+
+	handleInventoryItems(command, commandData) {
+		let inventory = player.getInventory()
+		for (let i = 0; i < inventory.length; i++) {
+			if (inventory[i].actions(command, commandData)) {
+				return true
+			}
+		}
+		return false
+	}
+
+	title(newTitle) {
+		document.title = newTitle
+	}
+
+	actions(command, commandData) {
+		if (command == "debug") {
+            let playerInventory = player.getInventory()
+            let playerPos = player.getPosition()
+            switch(commandData[0]) {
+                case "inventory":
+                    this.log("player inventory:", playerInventory)
+                    break;
+                case "room":
+                    this.log("room data:", world.getRoom(playerPos.zone, playerPos.room))
+				    this.log("room items:", world.getItems(playerPos.zone, playerPos.room))
+                    break;
+                default:
+                    this.log("player inventory:", playerInventory)
+                    this.log("room data:", world.getRoom(playerPos.zone, playerPos.room))
+				    this.log("room items:", world.getItems(playerPos.zone, playerPos.room))
+                    break;
+            }
+            return true
+
+		}
+		return false
+	}
+}
+
+var world = new World()
+var player = new Player()
+var system = new System()
+
