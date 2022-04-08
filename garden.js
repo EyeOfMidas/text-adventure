992069b3b58acca0bbf35c09185205924fd84425
Justin Gilman <eyeofmidas@gmail.com>
2022-04-08 09:20:53 -0400

    adding execute permissions to server.js; adding gardeners shed to garden

--- garden.js
+++ garden.js
@@ -6,7 +6,7 @@ export class Garden_Patio extends RoomCore {
 		super(
 			"A lush outdoor garden",
 			"The warmth of the sun falls on the brick patio, radiating heat in the walled garden. The smell of vines and flowers permeate the air.",
-			"An old oak door opens into the house to the <strong>west</strong>.",
+			"The rickety garden shed is to the <strong>south</strong>. An old oak door opens into the house to the <strong>west</strong>.",
 		)
 	}
 
@@ -15,6 +15,30 @@ export class Garden_Patio extends RoomCore {
 		system.println("")
 		system.println("You blink as your eyes adjust to the darkness.")
 		player.setPosition(OldHouse, OldHouse_DarkenedHallway)
+		return true
+	}
+
+	south() {
+		system.println("You enter the shed.")
+		system.println("")
+		player.setPosition(Garden, Garden_Shed)
+		return true
+	}
+}
+
+export class Garden_Shed extends RoomCore {
+	constructor() {
+		super(
+			"The gardener's shed",
+			"Rough hewn wood planks make up the walls of this small shed. There are shelves with various tools and gardening implements lining both walls. The workbench along the back wall has some potting soil spilled on it.",
+			"Sunlight streams in from the old wooden door frame to the <strong>north</strong>.",
+		)
+	}
+
+	north() {
+		system.println("You walk north, into the garden.")
+		system.println("")
+		player.setPosition(Garden, Garden_Patio)
 		return true
 	}
 }
@@ -27,12 +51,26 @@ export class GardenItems_Pansy extends ItemCore {
 			"The small purple pansy looks limp.",
 			"You carefully put the flower in your pocket.",
 			"a flower",
-			"You let the flower fall to the floor."
+			"You let the flower fall to the floor.",
+		)
+	}
+}
+
+export class GardenItems_WateringCan extends ItemCore {
+	constructor() {
+		super(
+			["can", "watering"],
+			"A rusted iron watering can stands on the ground.",
+			"The watering can still has a little bit of water in it.",
+			"You heft the watering can.",
+			"a watering can",
+			"You place the watering can on the ground.",
 		)
 	}
 }
 
 export class Garden{}
 
-world.addZone(Garden, [Garden_Patio]);
+world.addZone(Garden, [Garden_Patio, Garden_Shed]);
 world.addItems(Garden, Garden_Patio, [GardenItems_Pansy]);
+world.addItems(Garden, Garden_Shed, [GardenItems_WateringCan]);
